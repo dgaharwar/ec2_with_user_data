c565a3f605ba96fd224ab53527b9d508184cc615
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2023-05-20 16:40:29 +0100

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,60 @@
+# This template is designed for deploying an EC2 instance from the Morpheus UI.
+
+################################
+##         Variables          ##
+################################
+
+variable "region" {}
+variable "access_key" {}
+variable "secret_key" {}
+
+variable "vpc" {
+  default = "vpc-33ac354e"
+}
+variable "security_groups" {
+  default = "sg-2b299333"
+}
+variable "key_name" {
+  default = "MorpheusApp"
+}
+variable "power_schedule" {
+  default = "on"
+}
+variable "subnet" {
+  default = "us-east-1f"
+}
+
+locals {
+  ec2_power_schedule = "on" != "null" ? "on" : var.power_schedule
+}
+
+data "aws_subnet" "subnet" {
+  availability_zone = var.subnet
+  vpc_id            = var.vpc
+}
+
+#################################
+##          Provider           ##
+#################################
+
+provider "aws" {
+    region = var.region
+    access_key = var.access_key
+    secret_key = var.secret_key
+}
+
+#################################
+##           Resources         ##
+#################################
+
+resource "aws_instance" "ec2" {
+  instance_type           = "t2.micro"
+  ami                     = "ami-053b0d53c279acc90"
+  subnet_id               = data.aws_subnet.subnet.id
+  vpc_security_group_ids  = [var.security_groups]
+  key_name                = var.key_name
+  tags = {
+    Name = "<%=instance.name%>"
+    PowerSchedule = local.ec2_power_schedule
+    }
+}
